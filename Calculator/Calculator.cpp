8734bea529df38c0ab9bfcc77a53a6d36a4b6532
Alex Osintsev <alexdogg31@gmail.com>
2019-12-02 21:51:09 +0200

    Added main file Calculator.cpp

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,10 @@
+﻿// Calculator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	return 0;
+}
